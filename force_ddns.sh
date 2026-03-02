9965d601a45bc26fe514064f44b4784c43867b29
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2026-03-02 21:52:54 +0800

    Add files via upload

---
+++ force_ddns.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Force DDNS update
+# Triggered by cron (weekly on Sunday 03:30)
+
+LOG_FILE="/jffs/ddns_force.log"  # 脚本内部日志，持久化
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Script started - forcing DDNS update" >> "$LOG_FILE"
+logger -t "DDNS_FORCE" "Script started - forcing DDNS update"
+
+service restart_httpd >> "$LOG_FILE" 2>&1 || echo "[$(date '+%Y-%m-%d %H:%M:%S')] ERROR: restart_httpd failed" >> "$LOG_FILE"
+nvram set ddns_update=1
+nvram commit
+service restart_ddns_le >> "$LOG_FILE" 2>&1 || echo "[$(date '+%Y-%m-%d %H:%M:%S')] ERROR: restart_ddns_le failed" >> "$LOG_FILE"
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Script finished - DDNS force triggered" >> "$LOG_FILE"
+logger -t "DDNS_FORCE" "Script finished - DDNS force triggered"
