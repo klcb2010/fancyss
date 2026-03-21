66b91fca674d53b2b16833bc37675f3a846a7efa
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2026-03-21 18:16:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -60,8 +60,8 @@ cp_rules(){
 	cp -rf ${CURR_PATH}/rules_ng/rules.json.js ${target}
 	
 	# 替换智能DNS配置文件
-    cp -f "${CURR_PATH}/smartdns_smrt_1.conf" "${target}/smartdns_smrt_1.conf"
-    cp -f "${CURR_PATH}/smartdns_smrt_2.conf" "${target}/smartdns_smrt_2.conf"
+   # cp -f "${CURR_PATH}/smartdns_smrt_1.conf" "${target}/smartdns_smrt_1.conf"
+   # cp -f "${CURR_PATH}/smartdns_smrt_2.conf" "${target}/smartdns_smrt_2.conf"
 	cp -f "${CURR_PATH}/smartdns_smrt_3.conf" "${target}/smartdns_smrt_3.conf"
 
 }
