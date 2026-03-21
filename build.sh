09a5fd06af9185b8156ab066139d972c9b740669
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2026-03-21 11:22:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,14 +26,14 @@ cp_rules(){
 	cp -rf ${CURR_PATH}/rules_ng/rotlist.txt ${target}
 	cp -rf ${CURR_PATH}/rules_ng/white_list.txt ${target}
 	
-	#追加 asuscomm.com 到 white_list.txt
-    # local whitelist_file="${target}/white_list.txt"
-    # 追加换行，防止内容黏连
-    #  printf "\n" >> "$whitelist_file"
-    # 确保 asuscomm.com 在白名单中，防止国外优先模式下远程管理失效
-    #   if ! grep -qxF "asuscomm.com" "$whitelist_file"; then
-    # echo "asuscomm.com" >> "$whitelist_file"
-    # fi
+	# 追加 asuscomm.com 到 white_list.txt
+     local whitelist_file="${target}/white_list.txt"
+    #   追加换行，防止内容黏连
+      printf "\n" >> "$whitelist_file"
+    #  确保 asuscomm.com 在白名单中，防止国外优先模式下远程管理失效
+       if ! grep -qxF "asuscomm.com" "$whitelist_file"; then
+     echo "asuscomm.com" >> "$whitelist_file"
+     fi
 	
 	cp -rf ${CURR_PATH}/rules_ng/black_list.txt ${target}
 	
