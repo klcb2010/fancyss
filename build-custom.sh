36036a4a0f45c2d8db23dae6dda2040d42da4b00
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2026-04-13 22:17:24 +0800

    Update build-custom.sh

--- build-custom.sh
+++ build-custom.sh
@@ -23,7 +23,10 @@ cp_rules(){
 	cp -rf ${CURR_PATH}/rules_ng/chnroute.txt ${target}
 	cp -rf ${CURR_PATH}/rules_ng/chnroute6.txt ${target}
 	cp -rf ${CURR_PATH}/rules_ng/rules.json.js ${target}
-}
+	# 覆盖 ss_update.sh 和自定义名单
+    cp -f "${CURR_PATH}/ss_update.sh" "${CURR_PATH}/fancyss/scripts/ss_update.sh"
+    cp -f "${CURR_PATH}/black_list.txt" "${target}/black_list.txt"
+    cp -f "${CURR_PATH}/white_list.txt" "${target}/white_list.txt"
 
 cp_rules_ng2(){
 	local src=${CURR_PATH}/rules_ng2
@@ -411,33 +414,34 @@ gen_folder(){
 }
 
 build_pkg() {
-local platform=$1
-local pkgtype=$2
-local release_type=$3
-if [ ${release_type} == "release" ];then
-echo "打包：fancyss_${platform}_${pkgtype}.tar.gz"
-tar -zcf ${CURR_PATH}/packages/fancyss_${platform}_${pkgtype}.tar.gz shadowsocks >/dev/null
-md5value=$(md5sum ${CURR_PATH}/packages/fancyss_${platform}_${pkgtype}.tar.gz|tr " " "\n"|sed -n 1p)
-cat >>${CURR_PATH}/packages/version_tmp.json.js <<-EOF
+    local platform=$1
+    local pkgtype=$2
+    local release_type=$3
+    if [ ${release_type} == "release" ];then
+        echo "打包：fancyss_${platform}_${pkgtype}.tar.gz"
+        tar -zcf ${CURR_PATH}/packages/fancyss_${platform}_${pkgtype}.tar.gz shadowsocks >/dev/null
+        md5value=$(md5sum ${CURR_PATH}/packages/fancyss_${platform}_${pkgtype}.tar.gz | tr " " "\n" | sed -n 1p)
+        cat >>${CURR_PATH}/packages/version_tmp.json.js <<-EOF
 ,"md5_${platform}_${pkgtype}":"${md5value}"
 EOF
-elif [ ${release_type} == "debug" ];then
-echo "打包：fancyss_${platform}_${pkgtype}_${release_type}.tar.gz"
-tar -zcf ${CURR_PATH}/packages/fancyss_${platform}_${pkgtype}_${release_type}.tar.gz shadowsocks >/dev/null
-fi
+    elif [ ${release_type} == "debug" ];then
+        echo "打包：fancyss_${platform}_${pkgtype}_${release_type}.tar.gz"
+        tar -zcf ${CURR_PATH}/packages/fancyss_${platform}_${pkgtype}_${release_type}.tar.gz shadowsocks >/dev/null
+    fi
 }
 
 papare(){
-	rm -f ${CURR_PATH}/packages/*
-	cp_rules
-	prepare_geodata_assets
-	cp_rules_ng2
-	sync_binary
-	cat >${CURR_PATH}/packages/version_tmp.json.js <<-EOF
-	{
-	"name":"fancyss"
-	,"version":"${VERSION}"
-	EOF
+    mkdir -p "${CURR_PATH}/packages"
+    rm -f ${CURR_PATH}/packages/*
+    cp_rules
+    prepare_geodata_assets
+    cp_rules_ng2
+    sync_binary
+    cat >${CURR_PATH}/packages/version_tmp.json.js <<-EOF
+{
+"name":"fancyss"
+,"version":"${VERSION}"
+EOF
 }
 
 finish(){
@@ -454,32 +458,16 @@ pack(){
 }
 
 make(){
-	papare
-	# --- for release ---
-	pack hnd full release
-	pack hnd lite release
-	pack hnd_v8 full release
-	pack hnd_v8 lite release
-	pack hnd lite release
-	pack qca full release
-	pack qca lite release
-	pack arm full release
-	pack arm lite release
-	pack mtk full release
-	pack mtk lite release
-	pack ipq32 full release
-	pack ipq32 lite release
-	pack ipq64 full release
-	pack ipq64 lite release
-	# --- for debug ---
-	pack hnd full debug
-	pack hnd_v8 full debug
-	pack qca full debug
-	pack arm full debug
-	pack mtk full debug
-	pack ipq32 full debug
-	pack ipq64 full debug
-	finish
+    papare
+    # --- 只打包 full 版本
+    pack hnd full release
+    pack hnd_v8 full release
+    pack qca full release
+    pack arm full release
+    pack mtk full release
+    pack ipq32 full release
+    pack ipq64 full release
+    finish
 }
 
 make
